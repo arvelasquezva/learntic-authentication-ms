d404cb198d54187335c5aa441e6ce235c7cc8f35
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-10-03 14:14:51 -0500

    Auth Route Added

--- source/controllers/token_controller.js
+++ source/controllers/token_controller.js
@@ -1,2 +1,18 @@
-'use strict';
-const Token = require('../models/token_model')
+"use strict";
+const Token = require("../models/token_model");
+const Service = require("../service/index");
+
+function tokenI(req, res) {
+    Token.findOne({ token: req.body.token }, (err, account) => {
+        if (err) return res.status(500).send({ message: `error: ${err}` });
+        if (!account)
+            return res.status(404).send({ message: "no existe el token" });
+        else {
+            return res.status(201).send({ message: "Existe el token" });
+        }
+    });
+}
+
+module.exports = {
+    tokenI,
+};
--- source/routes/index.js
+++ source/routes/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const api = express.Router();
 const accountCtrl = require('../controllers/account_controller');
+const tokenCtrl = require('../controllers/token_controller');
 
 
 api.post('/signUp', accountCtrl.signUp);
 api.post('/signIn', accountCtrl.signIn);
+api.post('/auth', tokenCtrl.tokenI);
 
 //api.post('/singOut', accountCtrl.singOut);
 
