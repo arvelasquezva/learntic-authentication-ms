07cdf729e8a21bcfaecc151cf2f6b4bac87500f4
Diego A. Velasquez Vargas <31147126+arvelasquezva@users.noreply.github.com>
2020-10-03 14:34:54 -0500

    Auth routed modified

--- source/controllers/token_controller.js
+++ source/controllers/token_controller.js
@@ -2,14 +2,17 @@
 const Token = require("../models/token_model");
 const Service = require("../service/index");
 
-function tokenI(req, res) {
-    Token.findOne({ token: req.body.token }, (err, account) => {
-        if (err) return res.status(500).send({ message: `error: ${err}` });
-        if (!account)
-            return res.status(404).send({ message: "no existe el token" });
-        else {
-            return res.status(201).send({ message: "Existe el token" });
-        }
+function tokenI(req, res, next) {
+    Token.findOne({ token: t }, (err, tokenF) => {
+        if (err) return res.status(500).send({
+            authorization: false,
+            message: `internal server error ${err}`
+        })
+        if (tokenF) return res.status(401).send({
+            authorization: false,
+            message: 'token en blacklist'
+        })
+        next();
     });
 }
 
